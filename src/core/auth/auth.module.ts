c6d6f23043c385c58fc63ba8eab84454eacad3d9
loliallen <maxim.kasakin@yahoo.com>
2023-11-21 08:11:21 +0300

    fixing

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { UserModule } from "src/modules/user/user.module";
 import { AuthController } from "./auth.controller";
 import { WalletModule } from "src/modules/wallet/wallet.module";
 import { MailModule } from "src/modules/mail/mail.module";
+import { CosmosModule } from "src/modules/cosmos/cosmos.module";
 
 @Module({
-  imports: [UserModule, WalletModule, MailModule],
+  imports: [UserModule, WalletModule, MailModule, CosmosModule],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
